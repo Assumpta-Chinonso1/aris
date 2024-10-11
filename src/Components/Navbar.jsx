ae0e542800d04eb32416d67eb93cc861459eb828
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-10-11 11:36:33 +0100

    New commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import './Navbar.css'
-import { assests } from '../assets/assets'
-
+// src/components/Navbar.js
+import React from "react";
+import "./Navbar.css"; 
+import { assests } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <img className='logo' src={assests.logo} alt="logo" />
-
-        <div className="socials">
-            <img src={assests.ig} alt="ig" />
-            <img src={assests.linkedin} alt="linkedin" />
-            <img src={assests.twitter} alt="twitter" />
-
-            <div className="resume">
-                <button className='btn'>Download</button>
-            </div>
-        </div>
-          <span className="title">
-            <h1>Aris</h1> 
-
-      <p>  Creative Personal CV / Resume / Virtual Business Card & Portfolio Template with workable contact formCreative Personal CV / Resume / Virtual Business Card & Portfolio Template with workable contact form </p>
-
-      </span>
+    <nav className="navbar">
+      <div className="navbar-brand">
+         <img src={assests.logo} alt="" className="navbar-logo"/>
+          
+        </div>   
+      <div className="navbar-socials">
+                 <img src={assests.ig} alt="ig" />
+                 <img src={assests.linkedin} alt="linkrdin" />
+                 <img src={assests.twitter} alt="twitter" />
       </div>
-  )
-}
 
-export default Navbar
+      <div className="navbar-download">
+        <a href="#download" className="download-btn">Download</a>
+      </div>
+
+      <div className="navbar-title">
+        <h1>Aris</h1>
+        <p>Creative Personal CV / Resume / Virtual Business Card & Portfolio 
+            Template with workable contact form</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
