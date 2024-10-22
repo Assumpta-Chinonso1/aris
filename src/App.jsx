021371d1fa99aeef54e8a07217196af37f202966
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-10-22 01:16:43 +0100

    New comit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import './App.css'
 import NavCard from './Components/NavCard';
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero';
+import { assests } from './assets/assets.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const demos = [
-    { title: 'Aris (Light Demo)', subtitle: 'I am a web designer'},
-    { title: 'Aris (Dark Demo)', subtitle: 'I am a web developer' },
-    { title: 'Aris (With BG Image)', subtitle: 'I create beautiful websites'},
-    { title: 'Aris (Arabic)', subtitle: 'With RTL support' },
-    { title: 'Aris (Dark Demo)', subtitle: 'I am a web developer' }
+    { title: 'Aris (shopping List)', subtitle: 'I am a web designer', images: assests.portfolio1, link: 'https://calm-quokka-9028e9.netlify.app/'},
+    { title: 'Aris (Todo list)', subtitle: 'I am a web developer', images: assests.portfolio2, link: 'https://musical-pony-5be72a.netlify.app/' },
+    { title: 'Aris (Two steps info)', subtitle: 'I create beautiful websites', images: assests.portfolio3, link:'https://zesty-crisp-491624.netlify.app/'},
+    { title: 'Aris (palindrome)', subtitle: 'With RTL support', images: assests.portfolio4, link: 'https://sparkly-sopapillas-750d35.netlify.app/'  },
+    { title: 'Aris (Chuck Noris Jokes)', subtitle: 'I am a web developer', images: assests.portfolio5, link:'https://thriving-fenglisu-7a85c6.netlify.app/'  },
+    { title: 'Aris (Submit click)', subtitle: 'I am a web developer', images: assests.portfolio6, link:'https://friendly-faloodeh-8a606c.netlify.app/' },
+    { title: 'Aris (Calculator)', subtitle: 'I am a web developer', images: assests.portfolio7, link:'https://comforting-entremet-df20d1.netlify.app/' }, 
+    { title: 'Aris (Food website)', subtitle: 'I am a web developer', images: assests.portfolio8, link:'https://musical-alfajores-03fa1b.netlify.app/' }
   ];
 
   return (
+    <Router>
     <div className="App">
        
      
@@ -27,11 +33,14 @@ function App() {
             key={index}
             title={demo.title}
             subtitle={demo.subtitle}
+            images={demo.images}
+            link={demo.link}
           
           />
         ))}
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,15 @@ import linkedin from './linkedin_icon.png'
 import twitter from './twitter_icon.png'
 import aris from './aris background.jpg'
 import project from './project.jpg'
+import portfolio1  from './portfolio 1.png'
+import portfolio2  from './portfolio 2.png'
+import portfolio3 from './portfolio 3.png'
+import portfolio4 from './portfolio 4.png'
+import portfolio5 from './portfolio 5.png'
+import portfolio6 from './portfolio 6.png'
+import portfolio7 from './portfolio 7.png'
+import portfolio8 from './portfolio 8.png'
+import portfolio9 from './portfolio 9.png'
 
 
 const assests = {
@@ -12,7 +21,20 @@ const assests = {
     linkedin,
     twitter,
     aris,
-    project
+    project,
+    portfolio1,
+    portfolio2,
+    portfolio3,
+    portfolio4,
+    portfolio5,
+    portfolio6,
+    portfolio7,
+    portfolio8,
+    portfolio9
 }
+
+
+
+
 
 export{assests}
